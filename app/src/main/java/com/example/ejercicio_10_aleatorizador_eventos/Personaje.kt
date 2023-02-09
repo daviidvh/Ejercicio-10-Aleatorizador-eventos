68b3e1a1c2e28d4f4bc96ff6256806b9227829d5
daviidvh <davvel833@alu.centroafuera.es>
2023-02-09 14:17:20 +0100

    Ejer12 commit1

--- app/src/main/java/com/example/ejercicio_10_aleatorizador_eventos/Personaje.kt
+++ app/src/main/java/com/example/ejercicio_10_aleatorizador_eventos/Personaje.kt
@@ -3,7 +3,7 @@ package com.example.ejercicio_10_aleatorizador_eventos
 
 class Personaje {
     var nombre =""
-    var mochila:Mochila= Mochila(100)
+    var mochila:Mochila= Mochila(10)
 
 
     constructor(nombre:String){
--- app/src/main/java/com/example/ejercicio_10_aleatorizador_eventos/pantallaMercader.kt
+++ app/src/main/java/com/example/ejercicio_10_aleatorizador_eventos/pantallaMercader.kt
@@ -1,11 +1,10 @@
 package com.example.ejercicio_10_aleatorizador_eventos
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.ejercicio_10_aleatorizador_eventos.databinding.ActivityPantallaMercaderBinding
-import com.example.ejercicio_10_aleatorizador_eventos.databinding.ActivityPantallaObjetoBinding
 
 class pantallaMercader : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,14 +12,65 @@ class pantallaMercader : AppCompatActivity() {
         val binding = ActivityPantallaMercaderBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.botonComprar.visibility = View.INVISIBLE
+        binding.botonVender.visibility = View.INVISIBLE
+        binding.botonCancelar.visibility = View.INVISIBLE
+
+        binding.textViewInfo.visibility=View.INVISIBLE
+        binding.editTextNumArticulos.visibility=View.INVISIBLE
+        binding.botonCarrito.visibility= View.INVISIBLE
+
         binding.botonComerciar.setOnClickListener(){
-            val intent= Intent(this, pantallaBlanco::class.java)
-            startActivity(intent)
+
+            binding.botonComerciar.visibility = View.INVISIBLE
+            binding.botonContinuarMer.visibility = View.INVISIBLE
+
+            binding.botonComprar.visibility = View.VISIBLE
+            binding.botonVender.visibility = View.VISIBLE
+            binding.botonCancelar.visibility = View.VISIBLE
+
+            //BOTON COMPRAR
+            binding.botonComprar.setOnClickListener(){
+                binding.textViewInfo.visibility=View.VISIBLE
+                binding.editTextNumArticulos.visibility=View.VISIBLE
+                binding.botonCarrito.visibility= View.VISIBLE
+                binding.botonCarrito.setOnClickListener(){
+
+                }
+
+            }
+
+
+            //BOTON VENDER
+            binding.botonVender.setOnClickListener() {
+                /*Quitar cosas de boton comprar */
+                binding.textViewInfo.visibility = View.INVISIBLE
+                binding.editTextNumArticulos.visibility = View.INVISIBLE
+                binding.botonCarrito.visibility= View.INVISIBLE
+            }
+
+            //BOTON CANCELAR
+            binding.botonCancelar.setOnClickListener(){
+                binding.botonComerciar.visibility = View.VISIBLE
+                binding.botonContinuarMer.visibility = View.VISIBLE
+                binding.botonComprar.visibility = View.INVISIBLE
+                binding.botonVender.visibility = View.INVISIBLE
+                binding.botonCancelar.visibility = View.INVISIBLE
+                /*Quitar cosas de boton comprar */
+                binding.textViewInfo.visibility=View.INVISIBLE
+                binding.editTextNumArticulos.visibility=View.INVISIBLE
+                binding.botonCarrito.visibility= View.INVISIBLE
+
+                /* Quitar cosas de boton vender*/
+
+
+            }
         }
 
         binding.botonContinuarMer.setOnClickListener(){
             val intent= Intent(this, MainActivity::class.java)
             startActivity(intent)
+
         }
     }
 }
